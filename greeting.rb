4e5fd299ee98e45f10c4fd7157a9f30770ab56ce
Khaled Hassan <khaledhassan45@gmail.com>
2020-11-17 15:15:57 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,13 @@
+def greeting
+  puts "Hello World"
+end
+
+def say_it_5_times
+  greeting
+  greeting
+  greeting
+  greeting
+  greeting
+end
+
+say_it_5_times
---
+++ if-statements.rb
@@ -0,0 +1,16 @@
+# run_code_inside = false
+# puts "Code before if...end"
+# if run_code_inside
+#  puts "code inside"
+# end
+# puts "Code after if...end"
+
+def say_it_5_times
+  puts "Hello World"
+  puts "Hello World"
+  puts "Hello World"
+  puts "Hello World"
+  puts "Hello World"
+end
+
+say_it_5_times
